72846ec0fb3569b8143e877f5b1e4cf31e61349d
Vandy1104 <53416476+Vandy1104@users.noreply.github.com>
2019-08-23 15:32:41 +1200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('my script is working');
+
+$(document).ready(function(){
+$('.fa-bars').click(function(){
+	$('.headerMenu').toggleClass('active');
+});
+
+  //$('#close').click(function(){
+  	//$('nav').hide();
+  //})
+
+});
